36bd961ededf916373fad0d1bf433c8b7a403847
diayan <diayans@outlook.com>
2021-07-01 13:24:49 +0000

    initial commit - hubtel ios interview

---
+++ Hubtel-iOS/HTImageView.swift
@@ -0,0 +1,28 @@
+//
+//  HTImageView.swift
+//  Hubtel-iOS
+//
+//  Created by diayan siat on 01/07/2021.
+//
+
+import UIKit
+
+class HTImageView: UIImageView {
+
+    let placeholderImage = UIImage(systemName: "avatar-placeholder")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        image              = placeholderImage
+        layer.cornerRadius = 20
+    }
+    
+}
---
+++ Hubtel-iOS/HTNameLabel.swift
@@ -0,0 +1,24 @@
+//
+//  HTNameLabel.swift
+//  Hubtel-iOS
+//
+//  Created by diayan siat on 01/07/2021.
+//
+
+import UIKit
+
+class HTNameLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    init(textAlignment: NSTextAlignment, fontsize: CGFloat) {
+//        textAlignment = textAlignment
+//        //self.font          = UIFont(descriptor: UIFontDescriptor, size: <#T##CGFloat#>)
+//    }
+}
